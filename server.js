7de6f6d6b84efd245899cc1263838970fef56ff7
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-01-26 18:40:09 -0500

    pushing stripe

--- server.js
+++ server.js
@@ -1,11 +1,61 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
+// const path = require("path");
+// const express = require("express");
+// const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+// const routes = require("./controllers");
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const sequelize = require("./config/connection");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+//const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Middlewares here
+app.use(express.json());
+//app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+//app.use(express.static(path.join(__dirname, "public")));
+
+// Routes here
+app.get("/", (req, res) => {
+  res.render("index", { stripe });
+});
+
+// Listen
+app.listen(8000, () => {
+  console.log("Server started at port 8000");
+});
+
+app.post("/api/create-checkout-session", async (req, res) => {
+  const { product } = req.body;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: product.name,
+          },
+          unit_amount: product.price * 100,
+        },
+        quantity: product.quantity,
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+  });
+  res.json({ id: session.id });
+});
